03338b034617c53c3743a90d8d684b2e58243223
none <none>
2021-03-05 10:44:46 +0000

    herringbone added

--- gear.py
+++ gear.py
@@ -2,13 +2,14 @@ from math import atan, asin, acos, tan, sin, cos, pi
 
 class gear:
     @staticmethod
-    def spur(mod,teeth,bore,width):
-        width = 5
-        teeth = 35
-        mod = 1
+    def herringBone(mod,teeth,bore,width,twist):
+        g = gear.spur(mod,teeth,bore,width/2,twist) 
+        g = g.union(gear.spur(mod,teeth,bore,width/2,twist).mirror(mirrorPlane="XY"))
+        return g
+    @staticmethod
+    def spur(mod,teeth,bore,width,twist):
         pcd = mod*teeth
-        bore = 3
-        log(pcd)
+        log(f"pcd: {pcd}")
         OD = mod*(teeth+2)
         if mod >1.25:
             ID = OD - (OD-pcd)*2.25
@@ -36,19 +37,26 @@ class gear:
         gear =  cq.Workplane("XY").circle(bore/2).circle(OD/2).extrude(width)
         
         for t in range(0,teeth):
-            gear = gear.cut(cq.Workplane("XY").spline(sideA,includeCurrent=False) \
+            a = cq.Workplane("XY").spline(sideA,includeCurrent=False) \
                 .lineTo(sideA[-1][0]/baseDia * ID,sideA[-1][1]/baseDia *ID) \
                 .lineTo(sideB[0][0]/baseDia * ID,sideB[0][1]/baseDia *ID) \
                 .lineTo(sideB[0][0],sideB[0][1]) \
-                .spline(sideB,includeCurrent=False).close().extrude(width) \
-                .rotate([0,0,0],[0,0,1],t/teeth*360))
-        
+                .spline(sideB,includeCurrent=False).close()
+                
+            if twist != 0:
+                gear = gear.cut(a.twistExtrude(width,angleDegrees=twist)\
+                                .rotate([0,0,0],[0,0,1],t/teeth*360))
+            else:
+                gear = gear.cut(a.extrude(width).rotate([0,0,0],[0,0,1],t/teeth*360))
         return gear
 
+
+#takes a few seconds to run on an 2019 i7
 mod = 1
-teeth = 44
+teeth = 22
 bore = 5
-width = 3
-g = gear.spur(mod,teeth,bore,width)
-cq.exporters.export(g,f'SpurGear-M{mod}-T{teeth}-W{width}-B{bore}.stl')
-cq.exporters.export(g,f'SpurGear-M{mod}-T{teeth}-W{width}-B{bore}.step')
+width = 10
+for twist in (10,-10):
+    g = gear.herringBone(mod,teeth,bore,width,twist)
+    cq.exporters.export(g,f'HBGear-M{mod}-T{teeth}-W{width}-B{bore}-T{twist}.stl')
+#cq.exporters.export(g,f'SpurGear-M{mod}-T{teeth}-W{width}-B{bore}.step')
