1854f3f2576a35d35d9e2d88ba622b413fa06f84
r <harg505@gmail.com>
2025-11-19 05:55:37 +0000

    further

--- home/gifts/c++.py
+++ home/gifts/c++.py
@@ -1,4 +1,5 @@
 from cadquery import Workplane as WP
+from cadquery.selectors import LengthNthSelector as NthL 
 from math import sin, cos, pi
 T = 20
 
@@ -16,7 +17,24 @@ blue = WP().polygon(6,200).extrude(T).rotate((0,0,0),(0,0,1),30)
 blue = blue.edges("|Z").fillet(20)
 blue = blue.cut(white)
 
-white = white.union(WP().polygon(6,250).extrude(T).rotate((0,0,0),(0,0,1),30))
+white = white.union(WP().polygon(6,230).extrude(T).rotate((0,0,0),(0,0,1),30))
 white = white.cut(blue)
 
+pts = white.faces(">Z").wires(NthL(0,directionMax=False)).val().sample(9*6)
+
+black = WP().rect(250,250).extrude(T)
+black = black.cut(WP().polygon(6,230).extrude(T).rotate((0,0,0),(0,0,1),30))
+
+for e,p in enumerate(pts[0]):
+    if e%9!=0:
+        white = white.cut(WP().moveTo(p.x,p.y).circle(5.5/2).extrude(T))
+        black = black.cut(WP().moveTo(p.x,p.y).circle(5.5/2).extrude(T))
+
+assy = cq.Assembly()
+
+assy.add(black,color=cq.Color(0,0,0))
+assy.add(white,color=cq.Color(1,1,1))
+assy.add(blue,color=cq.Color(0,0,1))
+
+show_object(assy)
 
